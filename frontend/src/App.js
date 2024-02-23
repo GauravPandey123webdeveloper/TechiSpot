0e65e6295c5855c4c273025b0c41fcb7bc481243
Keshav <keshavagarwalfabs@gmail.com>
2024-02-23 18:57:12 +0530

    NavBar Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
+import "./App.css";
 import NavBar from "./component/NavBar";
+import "./component/navbar.css";
 import Post from "./component/Post";
+
 function App() {
   return (
-   <>
-   <NavBar/>
-   <Post/>
-   </>
+    <>
+      <NavBar />
+      <Post />
+    </>
   );
 }
 
--- frontend/src/component/NavBar.jsx
+++ frontend/src/component/NavBar.jsx
@@ -1,31 +1,40 @@
+import meme from "./icons/meme.png";
+import news from "./icons/news.png";
+import note from "./icons/notes.png";
+import tech from "./icons/tech.png";
+import home from "./icons/home.png";
+
 export default function NavBar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            TechiSpot
-          </a>
-          <div className="" id="">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  TechNews
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  TechMemes
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  TechNotes
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <nav className="navigation">
+        <img src={tech} alt="tech" className="icn"></img>
+        <div className="logo-name">TechiSpot</div>
+        <ul class="ul1">
+          <li class="lia">
+            <a href="/">
+              {" "}
+              <img src={home} alt="home" className="icon"></img>
+              Home
+            </a>
+          </li>
+          <li class="lib">
+            <a href="/">
+              <img src={news} alt="news" className="icon"></img>
+              Tech News
+            </a>
+          </li>
+          <li class="lic">
+            <img src={meme} alt="Meme" className="icon"></img>
+            <a href="/">
+              <meme></meme> Tech Memes
+            </a>
+          </li>
+          <li class="lid">
+            <img src={note} alt="notes" className="icon"></img>
+            <a href="/">Tech Notes</a>
+          </li>
+        </ul>
       </nav>
     </>
   );
