4c798d388afe2afc38cce738757ed89bdf0583d4
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-02-23 17:12:07 +0530

    addingIcons

--- frontend/src/component/NavBar.jsx
+++ frontend/src/component/NavBar.jsx
@@ -1,32 +1,14 @@
+import React from 'react'
+import './NavBar.css'
 export default function NavBar() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            TechiSpot
-          </a>
-          <div className="" id="">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  TechNews
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  TechMemes
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  TechNotes
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+    <div className='navbar'>
+      <ul className='navItems'>
+        <li className='list'><i class="fa-solid fa-house"></i> Techispot</li>
+        <li className='list'><i class="fa-solid fa-newspaper"></i> TechNews</li>
+        <li className='list'><i class="fa-regular fa-face-laugh-squint"></i> TechMemes</li>
+        <li className='list'> <i class="fa-solid fa-note-sticky"></i>TechNotes</li>
+      </ul>
+    </div>
+  )
 }
