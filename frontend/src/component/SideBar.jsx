c4255f9ff6c57397c60995141e30f024cf858562
ASHISHBVB <ashishdhamane3333@gmail.com>
2024-02-29 04:35:19 +0530

    [fix] reponsiveness

--- frontend/src/component/SideBar.jsx
+++ frontend/src/component/SideBar.jsx
@@ -12,7 +12,28 @@ export default function SideBar() {
             <li className={styles.courseItem}>Java</li>
             <li className={styles.courseItem}>DSA</li>
             <li className={styles.courseItem}>github</li>
-            <li className={styles.courseItem}>version control</li>
+            <li className={styles.courseItem}>course 1</li>
+            <li className={styles.courseItem}>course 2</li>
+            <li className={styles.courseItem}>course 3</li>
+            <li className={styles.courseItem}>course 4</li>
+            <li className={styles.courseItem}>course 5</li>
+            <li className={styles.courseItem}>course 6</li>
+            <li className={styles.courseItem}>course 7</li>
+            <li className={styles.courseItem}>course 8</li>
+            <li className={styles.courseItem}>course 9</li>
+            <li className={styles.courseItem}>course 10</li>
+            <li className={styles.courseItem}>course 11</li>
+            <li className={styles.courseItem}>course 12</li>
+            <li className={styles.courseItem}>course 13</li>
+            <li className={styles.courseItem}>course 14</li>
+            <li className={styles.courseItem}>course 15</li>
+            <li className={styles.courseItem}>course 16</li>
+            <li className={styles.courseItem}>course 17</li>
+            <li className={styles.courseItem}>course 18</li>
+            <li className={styles.courseItem}>course 19</li>
+            <li className={styles.courseItem}>course 20</li>
+            <li className={styles.courseItem}>course 21</li>
+            
         </ul>
     </div>
   )
--- frontend/src/component/Team.jsx
+++ frontend/src/component/Team.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 import styles from "./Team.module.css";
 export default function Team() {
   return (
+   
     <div className={styles.team}>
-        Team
+       <b><i>DEVELOPERS TEAM</i></b>
+       <ul><img src="https://wallpapercave.com/wp/wp4834844.jpg" alt="dev image" className={styles.imagelist}/></ul>
+        Senior Developer
+       <ul><img src="https://wallpapercave.com/wp/wp4834844.jpg" alt="dev image" className={styles.imagelist}/></ul>
+       Junior Developer
+       <ul><img src="https://wallpapercave.com/wp/wp4834844.jpg" alt="dev image" className={styles.imagelist}/></ul>
+       Junior Developer
+       <ul><img src="https://wallpapercave.com/wp/wp4834844.jpg" alt="dev image" className={styles.imagelist}/></ul>
+       Junior Developer
+       <ul><img src="https://wallpapercave.com/wp/wp4834844.jpg" alt="dev image" className={styles.imagelist}/></ul>
+       Junior Developer
+       <ul><img src="https://wallpapercave.com/wp/wp4834844.jpg" alt="dev image" className={styles.imagelist}/></ul>
+       Junior Developer
     </div>
+   
   )
 }
