4a45313bc3feeacba6737c937f23a7b410fb3b71
Overhere7 <23f2000090@ds.study.iitm.ac.in>
2024-02-29 12:59:24 +0530

    breaking news

--- frontend/src/news/News.jsx
+++ frontend/src/news/News.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
-import NavBar from '../component/NavBar'
+import React from "react";
+import NavBar from "../component/NavBar";
+import styles from "./News.module.css";
 
 export default function News() {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+      <div className={styles.tech}></div>
+      <div className={styles.techPost}></div>
+      <div className={styles.breakingNews}>
+        <h1 className={styles.heading}>
+          <i class="fa-solid fa-fire"></i> Breaking News
+          <i class="fa-solid fa-fire"> </i>
+        </h1>
+        <div className={styles.breaking_news_post}>
+          <ul className={styles.breaking_list}>
+            <li className={styles.breaking}>ChatGpt</li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>{" "}
+            <li className={styles.breaking}>ChatGpt </li>
+            <li className={styles.breaking}>World News </li>
+            <li className={styles.breaking}>Bitcoin News </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
