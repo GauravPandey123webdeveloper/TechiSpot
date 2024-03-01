5b5871a90b815aa30caa3046e55db99c5b9ef256
Ayush Srivastava <ayushsrivastavawork@gmail.com>
2024-03-01 22:57:33 +0530

    Added Styling to Headings of team amd community

--- frontend/src/component/Team.jsx
+++ frontend/src/component/Team.jsx
@@ -4,7 +4,7 @@ import TeamCard from "./our_Team_Data/TeamCard";
 export default function Team() {
   return (
     <div className={styles.team}>
-        <div className={styles.h1}><h1>Our Team</h1></div>
+        <div className={styles.h1}><h1 className={styles.teamhead}>Our Team</h1></div>
         <div className={styles.cards}>
       <TeamCard/>
     </div>
