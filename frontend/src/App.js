7e33f9929db34b96fd53ca2ac32154f8ddcdf42c
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-03-01 11:43:32 +0530

    app.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes ,Route } from "react-router-dom";
 import Home from "./home/Home";
 import News from "./news/News";
 import Meme from "./memes/Meme";
 import Notes from "./notes/Notes";
-
+import Sign from './sign/Sign'
+import Signup from "./signup/Signup";
 import CssNote from "./CssNotes/CssNote";
 import Error from "./error/Error";
 function App() {
@@ -16,6 +17,8 @@ function App() {
         <Route path="/notes" element={<Notes/>}/>
         <Route path="*" element={<Error/>}/>
         <Route path="/notes/css" element={<CssNote/>}/>
+        <Route path="/sign" element={<Sign/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
       
     </>
