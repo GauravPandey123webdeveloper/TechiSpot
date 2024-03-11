44db5f2649b81abfb7947931de6e724215322850
Adnan Khan <beingadnankhan678@gmail.com>
2024-03-11 13:05:08 +0530

    NavLink Added

--- frontend/src/error/Error.jsx
+++ frontend/src/error/Error.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './Error.module.css';
+import NavBar from '../component/NavBar';
 export default function Error() {
   return (
     <div className={styles.errorContainer}>
     <div className={styles.errorMessage}>
+    <NavBar/>
       <h1>Error 404: Page not found</h1>
       <p>Sorry, the page you are looking for might be under construction or does not exist.</p>
     </div>
