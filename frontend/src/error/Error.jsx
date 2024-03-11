e36fd1d2dd3638a77551c59d12beb8f151d48794
Adnan Khan <beingadnankhan678@gmail.com>
2024-03-11 17:28:51 +0530

    Updated

--- frontend/src/error/Error.jsx
+++ frontend/src/error/Error.jsx
@@ -3,12 +3,14 @@ import styles from './Error.module.css';
 import NavBar from '../component/NavBar';
 export default function Error() {
   return (
+    <>
+    <NavBar/>
     <div className={styles.errorContainer}>
     <div className={styles.errorMessage}>
-    <NavBar/>
       <h1>Error 404: Page not found</h1>
       <p>Sorry, the page you are looking for might be under construction or does not exist.</p>
     </div>
   </div>
+  </>
   )
 }
