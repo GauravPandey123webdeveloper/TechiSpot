c81e159a8d5a08bb1523d25e013bddb72bbf586d
Prateek-Fst <choudharyprateek131@gmail.com>
2024-03-12 09:49:59 +0530

    updated-techmemes-section

--- frontend/src/memes/Meme.jsx
+++ frontend/src/memes/Meme.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NavBar from '../component/NavBar'
 import styles from './Meme.module.css'
 import SideNavMeme from './SideNavMeme'
+import TechMemesPost from './TechMemesPost'
 
 export default function Meme() {
   return (
     <div className={styles.meme}>
       <NavBar />
       <SideNavMeme/>
+      <TechMemesPost/>
     
     </div>
   )
