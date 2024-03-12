9cd1f021cd6789dc6544223e9edf54bdad701cc4
Gaurav Pandey <130306260+GauravPandey123webdeveloper@users.noreply.github.com>
2024-03-12 16:20:49 +0530

    Revert "Technews"

--- frontend/src/news/NewsSideBar/Comment.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Comment() {
-    return (
-        <div>
-            <textarea cols="30" rows="10" ></textarea>
-        </div>
-    )
-}
--- frontend/src/news/NewsSideBar/NewsSideBar.jsx
+++ frontend/src/news/NewsSideBar/NewsSideBar.jsx
@@ -4,27 +4,27 @@ import styles from './NewsSideBar.module.css';
 export default function NewsSideBar() {
   return (
     <div className={styles.sideNav}>
-      <h1 className={styles.navHeading}><i class="fa-solid fa-fire-flame-curved"></i>Breaking News</h1><hr />
-      <ul className={styles.sideMenu}>
-        <li className={styles.techItems}><i class="fa-solid fa-robot"></i> Artificial Intelligence(AI)</li>
-        <li className={styles.techItems}><i class="fa-solid fa-shield-halved"></i> Cybersecurity</li>
-        <li className={styles.techItems}><i class="fa-brands fa-hive"></i> Blockchain</li>
-        <li className={styles.techItems}><i class="fa-brands fa-bitcoin"></i> Cryptocurrency</li>
-        <li className={styles.techItems}><i class="fa-solid fa-network-wired"></i>5G Networks</li>
-        <li className={styles.techItems}><i class="fa-brands fa-space-awesome"></i> Space Exploration</li>
-        {/*  */}
-        <li className={styles.techItems}><i class="fa-solid fa-wifi"></i> Internet of Things (IoT)</li>
-        <li className={styles.techItems}><i class="fa-solid fa-cloud-bolt"></i> Cloud Computing</li>
-        <li className={styles.techItems}><i class="fa-solid fa-vr-cardboard"></i> Augmented Reality (AR)</li>
-        <li className={styles.techItems}> <i class="fa-solid fa-vr-cardboard"></i> Virtual Reality (VR)</li>
-        <li className={styles.techItems}><i class="fa-solid fa-dna"></i> Biotechnology</li>
-        <li className={styles.techItems}><i class="fa-solid fa-user-doctor"></i> Health Tech</li>
-        <li className={styles.techItems}><i class="fa-solid fa-car"></i>Autonomous Vehicles</li>
-        <li className={styles.techItems}><i class="fa-solid fa-globe"></i> Digital Transformation</li>
-        <li className={styles.techItems}><i class="fa-solid fa-database"></i> Big Data and Analytics</li>
-        <li className={styles.techItems}><i class="fa-solid fa-globe"></i> Quantum Computing</li>
-        <li className={styles.techItems}><i class="fa-solid fa-globe"></i> Edge Computing</li>
-      </ul>
+        <h1 className={styles.navHeading}><i class="fa-solid fa-fire-flame-curved"></i>Breaking News</h1><hr/>
+       <ul className={styles.sideMenu}>
+          <li className={styles.techItems}><i class="fa-solid fa-robot"></i> Artificial Intelligence(AI)</li>
+          <li className={styles.techItems}><i class="fa-solid fa-shield-halved"></i> Cybersecurity</li>
+          <li className={styles.techItems}><i class="fa-brands fa-hive"></i> Blockchain</li>
+          <li className={styles.techItems}><i class="fa-brands fa-bitcoin"></i> Cryptocurrency</li>
+          <li className={styles.techItems}><i class="fa-solid fa-network-wired"></i>5G Networks</li>
+          <li className={styles.techItems}><i class="fa-brands fa-space-awesome"></i> Space Exploration</li>
+          {/*  */}
+          <li className={styles.techItems}><i class="fa-solid fa-wifi"></i> Internet of Things (IoT)</li>
+          <li className={styles.techItems}><i class="fa-solid fa-cloud-bolt"></i> Cloud Computing</li>
+          <li className={styles.techItems}><i class="fa-solid fa-vr-cardboard"></i> Augmented Reality (AR)</li>
+          <li className={styles.techItems}> <i class="fa-solid fa-vr-cardboard"></i> Virtual Reality (VR)</li>
+          <li className={styles.techItems}><i class="fa-solid fa-dna"></i> Biotechnology</li>
+          <li className={styles.techItems}><i class="fa-solid fa-user-doctor"></i> Health Tech</li>
+          <li className={styles.techItems}><i class="fa-solid fa-car"></i>Autonomous Vehicles</li>
+          <li className={styles.techItems}><i class="fa-solid fa-globe"></i> Digital Transformation</li>
+          <li className={styles.techItems}><i class="fa-solid fa-database"></i> Big Data and Analytics</li>
+          <li className={styles.techItems}><i class="fa-solid fa-globe"></i> Quantum Computing</li>
+          <li className={styles.techItems}><i class="fa-solid fa-globe"></i> Edge Computing</li>
+       </ul>
     </div>
   )
 }
